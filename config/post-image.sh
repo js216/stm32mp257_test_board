8c2f964f2640cd11c2e91130673f64b67f107478
Jakob Kastelic <kastelic.jakob@gmail.com>
2026-06-05 19:05:45 -0700

    make it pass again

---
+++ config/post-image.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+st_common="${BR2_EXTERNAL_ST_PATH}/board/stmicroelectronics/common"
+
+"${st_common}/generate-sdcard.sh" "$@"
+"${st_common}/generate-flashlayout.sh" "$@"
+
+tfa_dir="${BUILD_DIR}/arm-trusted-firmware-custom"
+usb_build_dir="${BUILD_DIR}/arm-trusted-firmware-usb"
+cross_compile="${HOST_DIR}/bin/aarch64-none-linux-gnu-"
+
+make -C "${tfa_dir}" -j"${PARALLEL_JOBS:-1}" \
+	CROSS_COMPILE="${cross_compile}" \
+	BUILD_STRING=custom \
+	BUILD_BASE="${usb_build_dir}" \
+	STM32MP_LPDDR4_TYPE=1 \
+	DTB_FILE_NAME=stm32mp257f-dk.dtb \
+	STM32MP_USB_PROGRAMMER=1 \
+	BL32_EXTRA2= \
+	BL33_CFG="${BINARIES_DIR}/u-boot.dtb" \
+	PLAT=stm32mp2 \
+	TARGET_BOARD= \
+	HOSTCC="${HOSTCC:-gcc}" \
+	ARM_ARCH_MAJOR=8 \
+	ARCH=aarch64 \
+	BL32="${BINARIES_DIR}/tee-header_v2.bin" \
+	BL32_EXTRA1="${BINARIES_DIR}/tee-pager_v2.bin" \
+	BL32_EXTRA2="${BINARIES_DIR}/tee-pageable_v2.bin" \
+	SPD=opteed \
+	BL33="${BINARIES_DIR}/u-boot-nodtb.bin" \
+	all fip
+
+cp -f "${usb_build_dir}/stm32mp2/release/tf-a-stm32mp257f-dk.stm32" \
+	"${BINARIES_DIR}/tf-a-stm32mp257_dk_usb.stm32"
+cp -f "${usb_build_dir}/stm32mp2/release/fip.bin" \
+	"${BINARIES_DIR}/fip-stm32mp257_dk_usb.bin"
+cp -f "${usb_build_dir}/stm32mp2/release/fip-ddr.bin" \
+	"${BINARIES_DIR}/fip-ddr-stm32mp257_dk_usb.bin"
